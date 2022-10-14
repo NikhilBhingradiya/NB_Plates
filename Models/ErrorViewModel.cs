e383fa326650e756c784039688dfad8e8b451404
NikhilBhingradiya <NB76@myscc.ca>
2022-10-14 13:31:56 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NB_Plates.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,2 @@
+﻿//10/14/2022
+My last app showing error So, I can decidedd to Creating a new NB_Plates  App
